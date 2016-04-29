f31399938178516c1feaa06efdbe2294e28f3df3
Mc <MacHome@Mc-MacBook-Pro.local>
2016-04-29 16:51:15 -0400

    Added Add Multiples app to your desire

--- infinitAddnMultiplyApp/ViewController.swift
+++ infinitAddnMultiplyApp/ViewController.swift
@@ -10,6 +10,91 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var addBy = 0
+    var currentVAlue = 0
+    var maxValue = 50
+    
+    
+    
+    
+    @IBOutlet var logoLAbel: UIImageView!
+    @IBOutlet var formulaLabel: UILabel!
+    
+    @IBOutlet var addMultiplesOfQuestion: UITextField!
+    
+    
+    @IBOutlet var startButton: UIButton!
+    
+    @IBOutlet var addButton: UIButton!
+    
+    
+    @IBAction func startButtonAction(sender: UIButton) {
+        
+        if Int(addMultiplesOfQuestion.text!) != 0 && addMultiplesOfQuestion.text != nil {
+            
+            addMultiplesOfQuestion.hidden = true
+            addButton.hidden = false
+            startButton.hidden = true
+            formulaLabel.hidden = false
+            
+            logoLAbel.hidden = true
+            
+            addBy = Int(addMultiplesOfQuestion.text!)!
+            
+            formulaLabel.text = "\(currentVAlue) + \(addBy) = \(currentVAlue + addBy)"
+           
+            currentVAlue = currentVAlue + addBy
+
+            
+            view.endEditing(true)
+
+            
+            
+            
+        }
+    }
+    
+    
+    func restrartGame(){
+        addBy = 0
+        currentVAlue = 0
+    
+        addMultiplesOfQuestion.hidden = false
+        addButton.hidden = true
+        startButton.hidden = false
+        formulaLabel.hidden = true
+        
+        logoLAbel.hidden = false
+        
+        addMultiplesOfQuestion.text = ""
+        
+        
+    }
+    
+    @IBAction func addAction(sender: UIButton) {
+        
+        if currentVAlue <= maxValue {
+            addBy = Int(addMultiplesOfQuestion.text!)!
+            
+            
+            formulaLabel.text = "\(currentVAlue) + \(addBy) = \(currentVAlue + addBy)"
+            
+            currentVAlue = currentVAlue + addBy
+        }else {
+        
+        restrartGame()
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
